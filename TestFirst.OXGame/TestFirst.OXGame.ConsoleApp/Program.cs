28765232964dc4687879088e466157eb92f08ff4
Sakul <au.perspectives@gmail.com>
2019-08-31 01:14:35 +0700

    Setup all projects.

---
+++ TestFirst.OXGame/TestFirst.OXGame.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestFirst.OXGame.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
