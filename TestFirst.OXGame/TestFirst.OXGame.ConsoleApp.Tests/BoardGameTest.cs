447d4465b26fbf075081c257da037e08ba968e88
Sakul <au.perspectives@gmail.com>
2019-08-31 01:24:35 +0700

    First test (fail).

---
+++ TestFirst.OXGame/TestFirst.OXGame.ConsoleApp.Tests/BoardGameTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+
+namespace TestFirst.OXGame.ConsoleApp.Tests
+{
+    public class BoardGameTest
+    {
+        [Fact(DisplayName = "ลง X ไปตอนที่กระดานว่าง ระบบให้ลงได้ แต่ยังไม่มีผู้ชนะ และสลับเป็นตาของ O")]
+        public void PlaceXWhenBoardIsEmpty()
+        {
+            var boardGame = new BoardGame();
+            var canPlace = boardGame.Plac("X", 0, 0);
+            Assert.True(canPlace);
+            Assert.Equal("O", boardGame.CurrentTurn);
+        }
+    }
+}
---
+++ TestFirst.OXGame/TestFirst.OXGame.ConsoleApp/BoardGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestFirst.OXGame.ConsoleApp
+{
+    public class BoardGame
+    {
+        public string CurrentTurn { get; set; }
+
+        public bool Plac(string symbol, int row, int column)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
